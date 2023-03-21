e5d2de98af245c10bd87041b0d7e7d2c2e145970
Lucas Nardon <lucaslnardon@gmail.com>
2023-03-21 04:38:07 -0300

    🚀 Add firebase deploy

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import * as Tone from "tone";
 import "./App.css";
 
 function App() {
-  function handleTone() {
+  function handleTone(note) {
     const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("D3", "8n");
+    synth.triggerAttackRelease(note, "8n");
   }
 
   return (
     <div className="App">
       <h1>3D Synth</h1>
       <h2>Coming Soon</h2>
-      <button onClick={handleTone}>Play Tone</button>
+      <button onClick={() => handleTone("C3")}>Play Tone C3</button>
+      <button onClick={() => handleTone("D3")}>Play Tone D3</button>
+      <button onClick={() => handleTone("E3")}>Play Tone E3</button>
+      <button onClick={() => handleTone("F3")}>Play Tone F3</button>
+      <button onClick={() => handleTone("G3")}>Play Tone G3</button>
     </div>
   );
 }
