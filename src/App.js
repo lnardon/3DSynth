25dfd44a62876a4d1322dfd7443bed47ec5aaa37
Lucas Nardon <lucaslnardon@gmail.com>
2023-03-21 04:29:17 -0300

    ✨ Add base tone play

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import * as Tone from "tone";
+
 import "./App.css";
 
 function App() {
+  function handleTone() {
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("D3", "8n");
+  }
+
   return (
     <div className="App">
       <h1>3D Synth</h1>
       <h2>Coming Soon</h2>
+      <button onClick={handleTone}>Play Tone</button>
     </div>
   );
 }
