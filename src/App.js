8880376678723faaf382cc1346970c6a18a4bca2
Lucas Nardon <lucaslnardon@gmail.com>
2023-03-22 01:07:56 -0300

    ✨ Add Three.js basic scene

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import * as Tone from "tone";
 
 import "./App.css";
@@ -6,6 +8,16 @@ function App() {
   function handleTone(note) {
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease(note, "8n");
+
+    // const synth = new Tone.MonoSynth({
+    //   oscillator: {
+    //     type: "sine",
+    //   },
+    //   envelope: {
+    //     attack: 0.1,
+    //   },
+    // }).toDestination();
+    // synth.triggerAttackRelease(note, "8n");
   }
 
   const keyboardInputToNote = {
@@ -34,6 +46,18 @@ function App() {
       <h1>3D Synth</h1>
       <h2>3D Coming Soon</h2>
       <h3>Use the keys from A to J to play the synth</h3>
+      <Canvas
+        camera={{ position: [0, 0, 2] }}
+        style={{ position: "absolute", top: 0 }}
+      >
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <mesh>
+          <boxGeometry args={[1, 1, 1, 1]} />
+          <meshStandardMaterial />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 }
