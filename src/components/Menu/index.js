d80b2bf5638e63c0c8a95a3c4d0be99235bf31f0
Lucas Nardon <lucaslnardon@gmail.com>
2023-03-22 07:37:16 -0300

    ✨ Add base menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h2>3D Synthesizer</h2>
+      <h4>Website under active development</h4>
+      <p>
+        Create and play with an interactive 3D synthesizer using Three.js and
+        Tone.js.
+      </p>
+      <a href="https://github.com/lnardon/3DSynth"> View on Github</a>
+    </div>
+  );
+}
+
+export default Menu;
