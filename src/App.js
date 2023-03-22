02d9189229562e7fe33dbbc10333a29ec0815140
Lucas Nardon <lucaslnardon@gmail.com>
2023-03-22 07:38:24 -0300

    ♻️ Adjust code for alpha deploy

--- src/App.js
+++ src/App.js
@@ -3,55 +3,58 @@ import { OrbitControls } from "@react-three/drei";
 import * as Tone from "tone";
 
 import Scene from "./components/Scene";
+import Menu from "./components/Menu";
 import "./App.css";
 
 function App() {
+  window.onload = () => {
+    alert(
+      "Click on the 3D synth keys to play. This is still under active development and will change in the future."
+    );
+  };
   function handleTone(note) {
-    const synth = new Tone.Synth().toDestination();
+    const vol = new Tone.Volume(-3).toDestination();
+    const synth = new Tone.Synth().connect(vol).toDestination();
     const targetNote =
       note.length === 4
         ? note.charAt(0) + note.charAt(note.length - 1)
         : note.charAt(0) + note.charAt(1) + note.charAt(note.length - 1);
-    synth.triggerAttackRelease(targetNote, "8n");
+    synth.triggerAttackRelease(targetNote, "4n");
   }
 
-  const keyboardInputToNote = {
-    KeyA: "C",
-    KeyW: "C#",
-    KeyS: "D",
-    KeyE: "D#",
-    KeyD: "E",
-    KeyF: "F",
-    KeyT: "F#",
-    KeyG: "G",
-    KeyY: "G#",
-    KeyH: "A",
-    KeyU: "A#",
-    KeyJ: "B",
-  };
+  // const keyboardInputToNote = {
+  //   KeyA: "C",
+  //   KeyW: "C#",
+  //   KeyS: "D",
+  //   KeyE: "D#",
+  //   KeyD: "E",
+  //   KeyF: "F",
+  //   KeyT: "F#",
+  //   KeyG: "G",
+  //   KeyY: "G#",
+  //   KeyH: "A",
+  //   KeyU: "A#",
+  //   KeyJ: "B",
+  // };
 
-  document.addEventListener("keypress", (e) => {
-    if (keyboardInputToNote[e.code]) {
-      handleTone(`${keyboardInputToNote[e.code]}`);
-    }
-  });
+  // document.addEventListener("keypress", (e) => {
+  //   if (keyboardInputToNote[e.code]) {
+  //     handleTone(`${keyboardInputToNote[e.code]}`);
+  //   }
+  // });
 
   return (
     <div className="App">
-      <h1>3D Synth</h1>
-      <h2>
-        Click on the 3D Synth keys to play ... or use your keyboard keys from A
-        to J to play the synth.
-      </h2>
       <Canvas
-        camera={{ position: [0, 0, 2] }}
+        camera={{ position: [0, 0.85, 0.95], rotation: [-0.77, 0, 0] }}
         style={{ position: "absolute", top: 0 }}
       >
         <ambientLight />
-        <pointLight position={[10, 10, 10]} />
+        <pointLight position={[0, 20, 3]} />
         <Scene handleKeyboardKeyPress={handleTone} />
-        <OrbitControls />
+        {/* <OrbitControls /> */}
       </Canvas>
+      <Menu />
     </div>
   );
 }
